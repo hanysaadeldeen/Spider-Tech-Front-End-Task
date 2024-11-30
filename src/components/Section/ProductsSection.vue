8dcbce8089e592939593abeed36c0abb8b45caff
Hany Mohamed <95915723+hanysaadeldeen@users.noreply.github.com>
2024-11-30 23:12:21 +0200

    Update ProductsSection.vue

--- src/components/Section/ProductsSection.vue
+++ src/components/Section/ProductsSection.vue
@@ -4,8 +4,7 @@
             img(:src="brands[0]?.img" alt="DUOBEL" loading="lazy" width="300" height="60")
             p(class='text-base font-normal text-[#737373] text-center') {{brands[0]?.desc}}
         ProductsContainer(:category="brands[0]?.categories")
-    div(v-else class="flex items-center justify-center h-48") 
-      div(class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900") 
+ 
 </template>
 
 <script setup lang="ts">
