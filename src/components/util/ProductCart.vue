b82a969a3b3d0616d49105ece5a3510ecd3f802d
Hany Mohamed <95915723+hanysaadeldeen@users.noreply.github.com>
2024-11-30 22:36:25 +0200

    Update ProductCart.vue

--- src/components/util/ProductCart.vue
+++ src/components/util/ProductCart.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     div(class='bg-[#F4F4F4] flex flex-col gap-6 rounded-2xl py-6 px-4 w-full')
-      img(:src='testImage[id] class='mx-auto' :alt="name" loading="lazy" width="252" height="252")
+      img(src="../../assets/images/folia-transferowa-silver-01.webp.svg" class='mx-auto' :alt="name" loading="lazy" width="252" height="252")
       h1(class="text-[#444444] text-[22px] font-medium text-center truncate " ) {{name}}
 </template>
 
@@ -14,14 +14,6 @@ interface Product {
 defineProps<Product>();
 
 
-    
-const testImage = [
-  "../../assets/images/folia-transferowa-silver-01.webp.svg",
-  "../../assets/images/new-duo-sugar-lemon.webp.svg",
-  "../../assets/images/new-duo-gabki-do-ombre.webp.svg",
-  "../../assets/images/paint-gel-5g-063-barbie.webp.svg",
-  "../../assets/images/folia-transferowa-silver-01.webp.svg",
-];
 </script>
 
 <style scoped></style>
